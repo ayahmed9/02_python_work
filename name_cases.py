f9cf1a6eec0c84d309152c5ff0efb4daca82fc53
Alvi Ahmed <ayahmed9@outlook.com>
2025-12-07 11:49:08 -0600

    Experimenting with comments.

--- name_cases.py
+++ name_cases.py
@@ -2,3 +2,7 @@ name = "zayn"
 print(name.lower())
 print(name.upper())
 print(name.title())
+
+# Do this in one line by utilizing character combination "\n"
+
+print(f"{name.lower()}\n{name.upper()}\n{name.title()}")
--- stripping_names.py
+++ stripping_names.py
@@ -1,2 +1,4 @@
 name = " zayn "
 print(f"\t{name.title()}\n\t{name.title().lstrip()}\n\t{name.title().rstrip()}\n{name.title().strip()}")
+
+# There are numerous ways to format the output utilizing the character combinations "\t" & "\n"
